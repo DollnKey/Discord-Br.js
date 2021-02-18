99799b2a287b287045301039d6e2dab9b2607fd4
Alvaro Matheus <xdbrgames10@gmail.com>
2021-02-18 11:00:27 -0300

    k

---
+++ index.d.ts
@@ -0,0 +1,56 @@
+import { EventEmitter } from "events";
+import Collection from "./src/Utils/Collection";
+
+declare function DiscordBr(options?: DiscordBr.options): DiscordBr.Client;
+
+declare namespace DiscordBr {
+
+    interface autor {
+        nome: string,
+        id: string,
+        hashtag: string,
+        avatar: string
+    }
+    
+    interface Message {
+        tipo: string;
+        criado: number;
+        conteudo: string;
+        servidorID: string;
+        canalID: string;
+        id: string;
+        autor: autor
+    }
+
+    interface EventListeners<T> {
+        (event: 'message', func: (msg: Message) => void): T;
+        (event: "ready", func: () => void): T;
+    }
+
+    interface ClientUser {
+        nome: string;
+        hashtag: string;
+        id: string;
+        verificado: boolean;
+        email: string;
+        bot: boolean;
+        flags: number;
+        avatar: string;
+    }
+
+    interface options {
+        formatoImagem: "png" | "jpg" | "jpeg" | "webp" | "gif"
+    }
+
+    export class Client extends EventEmitter{
+        eu: ClientUser;
+        tempoon: number;
+        token: string;
+        options: options;
+        servidores: Collection;
+        on: EventListeners<this>;
+        emit: EventListeners<this>
+    }
+}
+
+export = DiscordBr
--- test.js
+++
@@ -1,3 +0,0 @@
-const dbr = require("discord-br.js")
-
-const client = new dbr.Client()
